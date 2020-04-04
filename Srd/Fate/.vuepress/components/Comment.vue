1761d0d2ea8599c0f41f033531ef1fc94d1f483a
Jakub "Dukolm" Červený <dukolm@gmail.com>
2020-04-04 16:47:16 +0200

    DFA component add

---
+++ Srd/Fate/.vuepress/components/Comment.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="component-comment">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Comment',
+        props: [],
+        data() {
+            return {};
+        }
+    }
+</script>
+
+
+<style lang="scss">
+    .component-comment {
+        border-left: 0.25rem solid blueviolet;
+        padding-left: 1rem;
+        color: blueviolet;
+    }
+</style>
---
+++ Srd/Fate/.vuepress/components/Ivy.vue
@@ -0,0 +1,22 @@
+<template>
+    <span class="component-ivy">
+        <slot></slot>
+    </span>
+</template>
+
+<script>
+    export default {
+        name: 'Ivy',
+        props: [],
+        data() {
+            return {};
+        }
+    }
+</script>
+
+
+<style lang="scss">
+    .component-ivy {
+        color: blue;
+    }
+</style>

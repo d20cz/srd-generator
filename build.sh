a4e384faba732a5fa1098203729c8b14be623ac9
Jakub "Dukolm" Červený <dukolm@gmail.com>
2020-05-24 00:54:01 +0200

    Uprava buildění do zipu pro deploy

--- build.sh
+++ build.sh
@@ -12,5 +12,12 @@ fi;
 vuepress build Srd/$SELECT_SRD;
 
 rm /var/www/html/build/$SELECT_SRD-$DEPLOY_VERSION.zip
-cd Srd/$SELECT_SRD/.vuepress/dist;
-zip -r /var/www/html/build/$SELECT_SRD-$DEPLOY_VERSION.zip *;
+cd build;
+mkdir public
+
+SRD_DEPLOY_DATE=$(date +"%d. %m. %Y")
+cp -r /var/www/html/Srd/$SELECT_SRD/.vuepress/dist/* /var/www/html/build/public
+find ./ -type f -exec sed -i -e "s/##SRD_DEPLOY_VERSION##/$DEPLOY_VERSION/g" {} \;
+find ./ -type f -exec sed -i -e "s/##SRD_DEPLOY_DATE##/$SRD_DEPLOY_DATE/g" {} \;
+zip -r /var/www/html/build/$SELECT_SRD-$DEPLOY_VERSION.zip public/*;
+rm -rf /var/www/html/build/public
